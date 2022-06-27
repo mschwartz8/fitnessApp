b793e7fade40414dba1a14154644db11f7042f6f
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-06-27 15:13:28 -0400

    added repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exercises: [],
+      isLoading: true
+    
+  }
+  }
+ 
+  
+  componentDidMount (){
+    fetch('https://wger.de/api/v2/exercise/')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoading: true,
+        exercises: json,
+      })
+    })
+  }
+
+ 
+  render() {
+    console.log(this.state.exercises.results, 'this is state')
+    if (!this.state.isLoading){
+      return <div>is loading...</div>
+    
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            {this.state.exercises.results?.map( exercise => (
+                <li key={exercise.id}>
+                Name: {exercise.name}
+                
+                </li>
+              ))}
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      );
+    }
+  }
 }
+
+
 
 export default App;
